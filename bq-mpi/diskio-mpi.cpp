f74b2a6320d7432b930afb7c32709a00f4a57d1d
Divakar Viswanath <tg456871@login1.ls4.tacc.utexas.edu>
2013-02-23 12:16:33 -0600

    A8-diskio-done-but-more-output

--- bq-mpi/diskio-mpi.cpp
+++ bq-mpi/diskio-mpi.cpp
@@ -2,7 +2,9 @@
 #include <cstdio>
 #include <cstdlib>
 #include "TimeStamp.hh"
+#include "StatVector.hh"
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 void mpi_initialize(int& rank, int& nprocs){
@@ -28,14 +30,231 @@ void write_mpi(void *data, long len,
 		    MPI_BYTE,
 		    datarep,
 		    MPI_INFO_NULL);
-  MPI_File_write_at(ofile,
-		    0,
-		    data,
-		    (int)len,
-		    MPI_BYTE,
-		    MPI_STATUS_IGNORE);
+  long displacement = 0;
+  int maxcount = 2000*1000*1000;
+  while(len > 0){
+    int count = (len<maxcount)?len:maxcount;
+    MPI_File_write_at(ofile,
+		      displacement,
+		      data,
+		      count,
+		      MPI_BYTE,
+		      MPI_STATUS_IGNORE);
+    displacement += count;
+    data = ((char *)data+count);
+    len -= count;
+  }
   MPI_File_close(&ofile);
 }
+
+void read_mpi(void *data, long len, 
+	       char *fname, long disp){
+  MPI_File ifile;
+  MPI_File_open(MPI_COMM_WORLD, 
+		fname, 
+		MPI_MODE_RDONLY,
+		MPI_INFO_NULL,
+		&ifile);
+  char datarep[200];
+  sprintf(datarep, "native");
+  MPI_File_set_view(ifile, 
+		    disp, 
+		    MPI_BYTE,
+		    MPI_BYTE,
+		    datarep,
+		    MPI_INFO_NULL);
+  long displacement = 0;
+  int maxcount = 2000*1000*1000;
+  while(len > 0){
+    int count = (len<maxcount)?len:maxcount;
+    MPI_File_read_at(ifile,
+		     displacement,
+		     data,
+		     count,
+		     MPI_BYTE,
+		     MPI_STATUS_IGNORE);
+    displacement += count;
+    data = ((char *)data+count);
+    len -= count;
+  }
+  MPI_File_close(&ifile);
+}
+
+class LustreFile{
+private:
+  char dir[200];
+  char fname[200];
+  int rank;
+  int nprocs;
+  long totalsize;
+  long localsize;
+public:
+  LustreFile(int ranki, int nprocsi,
+	     const char *diri, const char *fnamei, long sizei);
+  long getlocalsize() {return localsize;}
+  void setstripe(int count);
+  void printinfo();
+  void write(double *v);
+  void read(double *v);
+};
+
+LustreFile::LustreFile(int ranki, int nprocsi,
+		       const char *diri, const char *fnamei, 
+		       long sizei){
+  rank = ranki;
+  nprocs = nprocsi;
+  strcpy(dir, diri);
+  strcpy(fname, fnamei);
+  totalsize = sizei;
+  localsize = totalsize/nprocs;
+  if(rank==(nprocs-1))
+    localsize = totalsize - (nprocs-1)*localsize;
+}
+
+void LustreFile::setstripe(int count){
+  if(rank==0){
+    char cmd[200];
+    sprintf(cmd, "rm %s/%s", dir, fname);
+    system(cmd);
+    sprintf(cmd, "lfs setstripe --count %d %s/%s", 
+	  count, dir, fname);
+    system(cmd);
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+}
+
+void LustreFile::printinfo(){
+  if(rank!=0)
+    return;
+  char cmd[200];
+  sprintf(cmd, "ls -l %s", dir);
+  system(cmd);
+  sprintf(cmd , "lfs getstripe %s/%s", dir, fname);
+  system(cmd);
+}
+
+void LustreFile::write(double *v){
+  long disp = 8*rank*(totalsize/nprocs);
+  char fnamex[200];
+  sprintf(fnamex, "%s/%s", dir, fname);
+  write_mpi((void *)v, 8*localsize, fnamex, disp);
+}
+
+void LustreFile::read(double *v){
+  long disp = 8l*rank*(totalsize/nprocs);
+  char fnamex[200];
+  sprintf(fnamex, "%s/%s", dir, fname);
+  read_mpi((void *)v, 8l*localsize, fnamex, disp);
+}
+
+void v_init(double *v, long len, int rank){
+  srand(rank+100);
+  for(long i=0; i < len; i++){
+    v[i] = (rand()*1.0/RAND_MAX-0.5)*2.0;
+  }
+}
+
+double v_avg(double *v, long len){
+  double avg = 0;
+  for(long i=0; i < len; i++)
+    avg += v[i];
+  avg /= len;
+  return avg;
+}
+
+struct BW_RW{
+  double bw_read;
+  double bw_write;
+};
+
+#define DIODIRNAME "diskio-mpi"
+
+//size = total size
+int time_lustre(int rank, int nprocs, long size, 
+		 struct BW_RW& bw, int stripecount=-1){
+  const double CPUGHZ=3.33;
+  TimeStamp clk;
+  char dir[200];
+  sprintf(dir, "%s/"DIODIRNAME,getenv("SCRATCH"));
+  if(rank==0){
+    char cmd[200];
+    sprintf(cmd, "mkdir %s", dir);
+    system(cmd);
+  }
+  char fname[200];
+  sprintf(fname, "parallel%d.dat", nprocs);
+  LustreFile lustre(rank, nprocs, dir, fname, size);
+  long lsize = lustre.getlocalsize();
+  double *v = new double[lsize];
+  v_init(v, lsize, rank);
+  double avg1 = v_avg(v, lsize);
+  if(stripecount==-1)
+    stripecount = (nprocs<=50)?nprocs:50;
+  lustre.setstripe(stripecount);
+  clk.tic();
+  lustre.write(v);
+  double cycles = clk.toc();
+  bw.bw_write = 8.0*size/cycles*CPUGHZ;
+  for(long i=0; i < lsize; i++)
+    v[i] = -1e100;
+  clk.tic();
+  lustre.read(v);
+  cycles = clk.toc();
+  bw.bw_read = 8.0*size/cycles*CPUGHZ;
+  double avg2 = v_avg(v, lsize);
+  cout<<"rank = "<<rank<<" diff = "<<avg1-avg2<<endl;
+  //if(rank==0)
+  //lustre.printinfo();
+  delete[] v;
+  return stripecount;
+}
+
+//flag==1 sets stripecount to 10
+void CreateOutputRW(int rank, int nprocs, int flag=0){
+  long nlist[2] = {1000l*1000, 2500l*1000*1000};//dbg
+  for(int i=0; i < 3; i++)//local to totalsize
+    nlist[i] *= nprocs;
+  ofstream ofile;
+  ofile.open("OUTPUT/diskio-mpi.txt",ios_base::app);
+  long posn = ofile.tellp();
+  if(posn <= 0 && rank==0){
+    char s[200];
+    sprintf(s, " nprocs\tstripes\tlocaln\t\twrite bw\tread bw (GB/s) (med/hgh)");
+    ofile<<s<<endl;
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+  struct BW_RW bw;
+  for(int i=0; i < 2; i++){
+    int stripecount;
+    int ntrials=5;
+    StatVector rstat(ntrials), wstat(ntrials);
+    for(int j=0; j < ntrials; j++){
+      if(flag==0)
+	stripecount = time_lustre(rank, nprocs, nlist[i], bw);
+      else
+	stripecount = time_lustre(rank, nprocs, nlist[i], bw, 20);
+      rstat.insert(bw.bw_read);
+      wstat.insert(bw.bw_write);
+    }
+    if(rank==0){
+      char s[200];
+      sprintf(s, " %d\t%d\t%7.2e\t%7.2e/%7.2e\t%7.2e/%7.2e",
+	      nprocs, stripecount, 1.0*nlist[i]/nprocs, 
+	      wstat.median(),wstat.max(),
+	      rstat.median(),rstat.max());
+      ofile<<s<<endl;
+    }
+  }
+  ofile.close();
+  if(rank==0){
+    char cmd[200];
+    sprintf(cmd, "ls -l %s/"DIODIRNAME, getenv("SCRATCH"));
+    system(cmd);
+    sprintf(cmd, "lfs getstripe %s/"DIODIRNAME"/*.dat", 
+	    getenv("SCRATCH"));
+    system(cmd);
+  }
+ }
 
 double runwrite(long count){
   int rank;
@@ -49,6 +268,10 @@ double runwrite(long count){
   if(rank==0){
     char cmd[200];
     sprintf(cmd, "rm %s", fname);
+    system(cmd);
+    //sprintf(cmd, "touch %s", fname);
+    //system(cmd);
+    sprintf(cmd, "lfs setstripe --count 20 %s", fname);
     system(cmd);
   }
   MPI_Barrier(MPI_COMM_WORLD);
@@ -68,5 +291,9 @@ double runwrite(long count){
 }
 
 int main(){
-  runwrite(1000l*1000*1000);
+  //runwrite(1000l*1000*1000*20);
+  int rank, nprocs;
+  mpi_initialize(rank, nprocs);
+  CreateOutputRW(rank, nprocs);
+  MPI_Finalize();
 }
--- bq-mpi/diskio-serial.cpp
+++ bq-mpi/diskio-serial.cpp
@@ -5,6 +5,7 @@
 #include "TimeStamp.hh"
 using namespace std;
 
+
 void write_easy(double *v, long len, const char *fname){
   ofstream ofile(fname);
   ofile<<scientific;
@@ -34,49 +35,98 @@ void read_direct(double *v, long len, const char *fname){
   fclose(fptr);
 }
 
-int main(){
-  double *v;
-  long n = 1000l*1000*10;
-  v = (double *)malloc(sizeof(double)*n*100);//*100 for later
-  for(long i=0; i < n*100; i++)//*100 for later
-    v[i] = i;
-  char fname[200];
+void v_init(double *v, long len){
+  for(long i=0; i < len; i++){
+    v[i] = (rand()*1.0/RAND_MAX-0.5)*2.0;
+  }
+}
+
+double v_avg(double *v, long len){
+  double avg = 0;
+  for(long i=0; i < len; i++)
+    avg += v[i];
+  avg /= len;
+  return avg;
+}
+
+struct BW_RW{
+  double bw_read;
+  double bw_write;
+};
+
+void time_diskio(const char *dir, long n, struct BW_RW& bw, int flag){
+  const double CPUGHZ=3.33;
   TimeStamp clk;
   double cycles;
-  
-  sprintf(fname, "%s/easy.dat", getenv("SCRATCH"));
-  clk.tic();
-  write_easy(v, n, fname);
-  cycles = clk.toc();
-  cout<<"n = "<<n<<endl;
-  cout<<"easy write b/w = "<<23.0*n/cycles<<" bytes/cycle"<<endl;
-  clk.tic();
-  read_easy(v, n, fname);
-  cycles = clk.toc();
-  cout<<"easy read b/w = "<<23.0*n/cycles<<" bytes/cycle"<<endl;
-  double sum = 0;
-  for(long i=0; i < n; i++)
-    sum+=v[i];
-  cout<<"error = "<<sum-1.0*(n-1)*n/2<<endl<<endl;
-
-  n *= 100;
-  cout<<"n = "<<n<<endl;
-  sprintf(fname, "%s/direct.dat", getenv("SCRATCH"));
-  clk.tic();
-  write_direct(v, n, fname);
-  cycles = clk.toc();
-  cout<<"direct write b/w = "<<8.0*n/cycles<<" bytes/cycle"<<endl;
-  read_direct(v, n, fname);
-  cycles = clk.toc();
-  cout<<"direct read b/w = "<<23.0*n/cycles<<" bytes/cycle"<<endl;
-  sum = 0;
-  for(long i=0; i < 10; i++)
-    cout<<v[i]<<" "<<v[n/2+i]<<" "<<v[n+i-100]<<endl;
-  for(long i=0; i < n; i++)
-    sum+=v[i];
-  cout<<"(rounding!) error = "<<sum-1.0*(n-1)*n/2<<endl<<endl;
-
+  double fac = (flag==0)?23.5:8.0;
+  char fname[200];
+  if(flag==0)
+    sprintf(fname, "%s/easy.dat", dir);
+  else
+    sprintf(fname, "%s/direct.dat", dir);
   char cmd[200];
-  sprintf(cmd,"ls -al %s", getenv("SCRATCH"));
+  sprintf(cmd, "rm %s", fname);
   system(cmd);
+  double *v = new double[n];
+  double *w = new double[n];
+  v_init(v, n);
+  clk.tic();
+  if(flag==0)
+    write_easy(v, n, fname);
+  else
+    write_direct(v, n, fname);
+  cycles = clk.toc();
+  bw.bw_write = fac*n/cycles*CPUGHZ;
+  clk.tic();
+  if(flag==0)
+    read_easy(w, n, fname);
+  else
+    read_direct(w, n, fname);
+  cycles = clk.toc();
+  bw.bw_read = fac*n/cycles*CPUGHZ;
+  double avgv = v_avg(v, n);
+  double avgw = v_avg(v, n);
+  cout<<"avgv/diff = "<<avgv<<"/"<<avgw-avgv<<endl;
+  delete[] v;
+  delete[] w;
+}
+
+void CreateOutputRW(){
+  char dir[200];
+  sprintf(dir, "%s/diskio-serial", getenv("SCRATCH"));
+  char cmd[200];
+  sprintf(cmd, "mkdir %s", dir);
+  system(cmd);
+  ofstream ofile;
+  for(int flag=0; flag < 2; flag++){
+    long nlist[3] = {1000l, 1000l*100, 1000l*100*100};
+    if(flag!=0){
+      nlist[1] *= 10;
+      nlist[2] *= 100;
+    }
+    if(flag==0)
+      ofile.open("OUTPUT/diskio-easy.txt", ios_base::app);
+    else
+      ofile.open("OUTPUT/diskio-direct.txt", ios_base::app);
+    long posn = ofile.tellp();
+    if(posn <= 0)
+      ofile<<"\t"<<"n"<<"\t\t"<<"write b/w"<<"\t\t"<<"read b/w (GB/s)"<<endl;
+    struct BW_RW bw;
+    for(int i=0; i < 3; i++){
+      time_diskio(dir, nlist[i], bw, flag);
+      ofile<<"\t"<<(double)nlist[i]<<"\t\t"<<bw.bw_write
+	   <<"\t\t"<<bw.bw_read<<endl;
+    }
+    ofile.close();
+  }
+  sprintf(cmd, "ls -l %s", dir);
+  system(cmd);
+  sprintf(cmd, "lfs getstripe %s/direct.dat", dir);
+  system(cmd);
+}
+
+
+
+int main(){
+  CreateOutputRW();
 }
