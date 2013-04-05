e2b77b2d70347574892b6c022fae9bc437caefba
Divakar Viswanath <divakar@HP-a6400z>
2013-04-05 10:43:25 -0400

    Modified altadd_omp to eliminate WORK_COUNT
    The effect of WORK_COUNT on futex locking is now in a separate branch.
    However, output from that branch is copied to this branch as well.

--- bk-pthreads/altadd_omp.cpp
+++ bk-pthreads/altadd_omp.cpp
@@ -7,7 +7,6 @@ using namespace std;
 #include "dvmesg.h"
 
 #undef DV_KERNEL_MESG
-#define WORK_COUNT 0
 
 const int nthreads = 4;
 const int nprocs = 4;
@@ -17,19 +16,11 @@ const int nprocs = 4;
 void addone(void *arg){
 	long *p = (long *)(arg);
 	*p += 1;
-#ifdef WORK_COUNT
-	for(int i=0; i < WORK_COUNT; i++)
-		*p += (*p)%37;
-#endif
 }
 
 void addtwo(void *arg){
 	long *p = (long *)(arg);
 	*p += 2;
-#ifdef WORK_COUNT
-	for(int i=0; i < WORK_COUNT; i++)
-		*p += rand();
-#endif
 }
 
 void ompmaster(long *list, int nthreads, int count){
@@ -56,13 +47,10 @@ void ompmaster(long *list, int nthreads, int count){
 int main(){
 	long list[nthreads]={0};
 	int count = (nthreads<=nprocs)?1000*1000*100:1000*100; 
-	if(WORK_COUNT > 0)
-		count = 1000*100*10;
 
 #ifdef DV_KERNEL_MESG
 	count = 6;
 	cout<<"tgid = "<<getpid()<<endl;
-	cout<<"WORK_COUNT = "<<WORK_COUNT<<endl;
 	ompmaster(list, nthreads, count);
 	set_dvflag(400);
 	ompmaster(list, nthreads, count);
@@ -77,7 +65,6 @@ int main(){
 	ofile<<"nprocs = "<<nprocs<<endl;
 	ofile<<"nthreads = "<<nthreads<<endl;
 	ofile<<"count = "<<count<<endl;
-	ofile<<"WORK_COUNT = "<<WORK_COUNT<<endl;
 	ofile<<"cycles per parallel region = "<<cycles/count<<endl;
 	ofile.close();
 #endif
