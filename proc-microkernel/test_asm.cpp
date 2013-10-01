2ad64c6534ae0a82413b280ca27329f3d2567c3b
Divakar Viswanath <divakar@HP-Z220>
2013-10-01 12:58:21 -0400

    test_asm.cpp for asm4xnx4 with n = 1, 4, 20, 40, 100, 200

--- proc-microkernel/test_asm.cpp
+++ proc-microkernel/test_asm.cpp
@@ -31,33 +31,63 @@ void easymult(double *restrict a, double *restrict b, double *restrict c,
 				c[i+k*ldc] += a[i+j*lda]*b[j+k*ldb];
 }
 
-void test4x1x4(){
-	__declspec(align(16)) double a[4];
-	__declspec(align(16)) double b[4];
+void test4xnx4(int n){
+	__declspec(align(16)) double a[4*n];
+	__declspec(align(16)) double b[4*n];
 	__declspec(align(16)) double c[16];
 
 	double cc[16];
 	for(int i=0; i < 16; i++)
 		c[i] = cc[i] = rand()*1.0/RAND_MAX-0.5;
-	for(int i=0; i < 4; i++){
+	for(int i=0; i < 4*n; i++){
 		a[i] = rand()*1.0/RAND_MAX-0.5;
 		b[i] = rand()*1.0/RAND_MAX-0.5;
 	}
 
 	 skew2x2(c, 4, 4);
-	 asm4x1x4(a, b, c);
+	 
+	 switch(n){
+	 case 1:
+		 asm4x1x4(a, b, c);
+		 break;
+	 case 4:
+		 asm4x4x4(a, b, c);
+		 break;	 
+	 case 20:
+		 asm4x20x4(a, b, c);
+		 break;
+	 case 40:
+		 asm4x40x4(a, b, c);
+		 break;
+	 case 100:
+		 asm4x100x4(a, b, c);
+		 break;	 
+	 case 200:
+		 asm4x200x4(a, b, c);
+		 break;
+	 case default:
+		 assrt(0 == 1);
+	 }
+	 /*
+	  * unskew by skewing twice
+	  */
 	 skew2x2(c, 4, 4);
 	 skew2x2(c, 4, 4);
-	 easymult(a, b, cc, 4, 1, 4);
+	 easymult(a, b, cc, 4, n, 4);
 	
 	 array_diff(c, cc, 16);
 	 double rerr = array_max(c, 16)/array_max(cc,16);
 
-	 printf("\t\tTesting asm4x1x4\n");
+	 printf("\t\tTesting asm4x%dx4\n", n);
 	 printf("\t\tinit with rand()\n");
-	 printf("\tRelative Error = %.2e", rerr);
+	 printf("\tRelative Error = %.2e\n\n", rerr);
 }
 
 int main(){
-	test4x1x4();
+	test4xnx4(1);
+	test4xnx4(4);
+	test4xnx4(20);
+	test4xnx4(40);
+	test4xnx4(100);
+	test4xnx4(200);
 }
