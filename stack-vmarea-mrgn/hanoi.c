6376a9ad2f9aec343c34c391863f80850114793c
Divakar Viswanath <divakar@HP-a6400z>
2013-10-23 10:50:02 -0400

    stack-vmarea-mrgn/hanoi.c ready to compile

---
+++ stack-vmarea-mrgn/hanoi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void hanoi(int from, int to, int n){
+	int other = 3-(from+to);
+	if(n==1)
+		printf("move from %d to %d \n", from, to);
+	else{
+		hanoi(from, other, n-1);
+		printf("move from %d to %d \n", from, to);
+		hanoi(other, to, n-1);
+	}
+}
+
+int main(){
+	int n = 5;
+	printf("n = ");
+	scanf("%d", &n);
+	hanoi(0, 1, n);
+}
