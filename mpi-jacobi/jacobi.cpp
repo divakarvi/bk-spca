f2407fdaac7ed47785efa942a11150de5d07b031
Divakar Viswanath <divakar@tacc>
2013-11-26 06:06:18 -0600

    simplified and compiled mpi-jacobi/jacobi

--- mpi-jacobi/jacobi.cpp
+++ mpi-jacobi/jacobi.cpp
@@ -12,26 +12,24 @@ Jacobi2D::Jacobi2D(int rank, int nprocs, int d1, int d2, int nth){
 	assrt(dim1%2 == 0);
 	assrt(dim2%2 == 0);
 
-	a = new double[dim1*dim2];
-	b = new double[dim1*dim2];
-	al = new double[dim1];
-	ar = new double[dim1];
-	bl = new double[dim1];
-	br = new double[dim1];
-	ll = new double[dim1];
-	rr = new double[dim1];
+	a = new double[dim1*(dim2+2)];
+	b = new double[dim1*(dim2+2)];
 
 	lrank = (rank==0)?nprocs-1:rank-1;
 	rrank = (rank==nprocs-1)?0:rank+1;
 
 	int tag = 0;
+	double *left = a;
+	double *al = a + dim1;
+	double *ar = a + dim1*dim2;
+	double *right = a + dim1*(dim2+1);
 	MPI_Send_init(al, dim1, MPI_DOUBLE, lrank, tag, 
 		      MPI_COMM_WORLD, reqlist+0);
 	MPI_Send_init(ar, dim1, MPI_DOUBLE, rrank, tag, 
 		      MPI_COMM_WORLD, reqlist+1);
-	MPI_Recv_init(ll, dim1, MPI_DOUBLE, lrank, tag, 
+	MPI_Recv_init(left, dim1, MPI_DOUBLE, lrank, tag, 
 		      MPI_COMM_WORLD, reqlist+2);
-	MPI_Recv_init(rr, dim1, MPI_DOUBLE, rrank, tag, 
+	MPI_Recv_init(right, dim1, MPI_DOUBLE, rrank, tag, 
 		      MPI_COMM_WORLD, reqlist+3);
 }
 
@@ -43,38 +41,34 @@ Jacobi2D::~Jacobi2D(){
 	MPI_Request_free(reqlist+3);
 	delete[] a;
 	delete[] b;
-	delete[] al;
-	delete[] ar;
-	delete[] bl;
-	delete[] br;
-	delete[] ll;
-	delete[] rr;
 }
 
 void Jacobi2D::initialize(int col1, int col2){//chessboard pattern
-	for(int j=col1; j < col2; j++)
+	for(int j=col1+1; j < col2+1; j++)
 		for(int i=0; i < dim1; i++){
-			a[i+j*dim1] = ((i+j)%2==0)?1:-1;
+			a[i+j*dim1] = ((i+j-1)%2==0)?1:-1;
 			b[i+j*dim1] = 0;
-		}
-	if(col1 == 0)
-		for(int i = 0; i < dim1; i++){
-			al[i] = ((i+1)%2==0)?1:-1;
-			ar[i] = (i%2==0)?1:-1;
-			bl[i] = 0;
-			br[i] = 0;
 		}
 }
 
 void Jacobi2D::initializepp(){
+	/*
+	 * omp does not see the class members dim2, nthreads
+	 * default(shared) + shared(dim2, nthreads) gives compilation error
+	 * way to get around 
+	 * --Jacobi2D *self = this;
+	 * --shared(this)
+	 * --self->dim2, self->nthreads
+	 * is a bit too clumsy
+	 */ 
 #pragma omp parallel				\
 	num_threads(nthreads)			\
-	default(none)					
+	default(shared)				
 	{
 		int tid = omp_get_thread_num();
 		int col1 = tid*dim2/nthreads;
 		int col2 = (tid+1)*dim2/nthreads;
-		this->initialize(col1, col2);
+		initialize(col1, col2);
 	}
 }
 
@@ -87,72 +81,39 @@ void Jacobi2D::wait(){
 }
 
 void Jacobi2D::updateinterior(int col1, int col2){
-	if(col1 == 0){
-		int j = 0;
+	for(int j=col1+1; j < col2+1; j++)
 		for(int i=0; i < dim1; i++){
-			int iup = (i==0)?(dim1-1):i-1;
-			int idown = (i==dim1-1)?0:i+1;
-			b[i+j*dim1] = 0.25*(a[iup+j*dim1]+a[idown+j*dim1]
-					    +al[i]+a[i+(j+1)*dim1]);
-		}
-		col1++;
-	}
-	if(col2==dim2){
-		int j = dim2-1; 
-		for(int i=0; i < dim1; i++){
-			int iup = (i==0)?(dim1-1):i-1;
-			int idown = (i==dim1-1)?0:i+1;
-			b[i+j*dim1] = 0.25*(a[iup+j*dim1]+a[idown+j*dim1]
-					    +a[i+(j-1)*dim1]+ar[i]);
-		}
-		col2--;
-	}
-	for(int j=col1; j < col2; j++)
-		for(int i=0; i < dim1; i++){
-			int iup = (i==0)?(dim1-1):i-1;
-			int idown = (i==dim1-1)?0:i+1;
-			b[i+j*dim1] = 0.25*(a[iup+j*dim1]+a[idown+j*dim1]
-					    +a[i+(j-1)*dim1]+a[i+(j+1)*dim1]);
+			int iup = (i+dim1-1)%dim1;
+			int idown = (i+1)%dim1;
+			b[i+j*dim1] = 0.25*(a[iup+j*dim1]
+					    + a[idown+j*dim1]
+					    + a[i+(j-1)*dim1]
+					    + a[i+(j+1)*dim1]);
 		}
 }
 
 void Jacobi2D::updateinteriorpp(){
 #pragma omp parallel				\
 	num_threads(nthreads)			\
-	default(none)					
+	default(shared)				
 	{
 		int tid = omp_get_thread_num();
 		int col1 = tid*dim2/nthreads;
 		int col2 = (tid+1)*dim2/nthreads;
-		this->updateinterior(col1, col2);
-  }
-}
-
-void Jacobi2D::updateboundary(){
-	for(int i=0; i < dim1; i++){
-		int iup = (i==0)?(dim1-1):i-1;
-		int idown = (i==dim1-1)?0:i+1;
-		bl[i] = 0.25*(al[iup]+al[idown]+ll[i]+a[i]);
-		br[i] = 0.25*(ar[iup]+ar[idown]+
-				  a[i+(dim2-1)*dim1]+rr[i]);
+		updateinterior(col1, col2);
 	}
 }
 
 void Jacobi2D::copy(int col1, int col2){
-	for(int j=col1; j < col2; j++)
+	for(int j=col1+1; j < col2+1; j++)
 		for(int i=0; i < dim1; i++)
 			a[i+j*dim1] = b[i+j*dim1];
-	if(col1 == 0)
-		for(int i=0; i < dim1; i++){
-			al[i] = bl[i];
-			ar[i] = br[i];
-		}
 }
 
 void Jacobi2D::copypp(){
 #pragma omp parallel				\
 	num_threads(nthreads)			\
-	default(none)					
+	default(shared)				
 	{
 		int tid = omp_get_thread_num();
 		int col1 = tid*dim2/nthreads;
--- mpi-jacobi/jacobi.hh
+++ mpi-jacobi/jacobi.hh
@@ -5,29 +5,18 @@
 class Jacobi2D{
 private:
 	int nthreads;
+	/*
+	 * dim1 x dim2 = dimension of data on a single node
+	 */
 	int dim1;
 	int dim2;
 	/*
-	 * a and b are both dim1 x dim2
+	 * a and b are both dim1 x (dim2+2)
+	 * extra column on left = rightmost column of left neighbor
+	 * extra column on right = leftmost column of right neighbor
 	 * each  iteration updates b[] using a[]
 	 */
 	double *a, *b;
-	/*
-	 * al = leftmost column supplementing a[] matrix
-	 * right = rightmost column supplementing a[] matrix
-	 * with the edges the dim at every node is dim1 x (dim2 + 2)
-	 */
-	double *al, *ar;
-	/*
-	 * ll = rightmost column of matrix to the left
-	 * rr = leftmost column of matrix to the right
-	 */
-	double *ll, *rr;
-	/*
-	 * bl = leftmost column supplementing b[] matrix
-	 * br = rightmost column supplementing b[] matrix
-	 */
-	double *bl, *br;
 	/*
 	 * lrank = rank of MPI process to the left
 	 * rrank = rank of MPI process to the right
@@ -47,8 +36,6 @@ public:
 	 * d1(dim1) must be even
 	 * d2(dim2) must be even
 	 * even ---> assumed by initializepp() to get chessboard pattern
-	 * dim2 divisible by nthreads ---> used to partition columns
-	 * (not needed?)
 	 */
 	Jacobi2D(int rank, int nprocs, int d1, int d2, int nth);
 	~Jacobi2D();
@@ -58,22 +45,18 @@ public:
 	void initializepp();
 	void postsendrecv();
 	/*
-	 * b[] updated using a[], al, ar
+	 * b[] updated using a[]
 	 */
 	void updateinteriorpp();
 	void wait();
 	/*
-	 * bl, br updated using al, ar, ll, rr,
-	 * and first and last columns of a[]
-	 */
-	void updateboundary();
-	/*
-	 * copy a[], al, ar to b[], bl, br
+	 * copy a[] to b[]
 	 */
 	void copypp();
-	double* geta(){return a;}
-	double* getal(){return al;}
-	double* getar(){return ar;}
+	/*
+	 * return pointer to data excluding column from left neighbor
+	 */
+	double* geta(){return a+dim1;}
 };
 
 #endif
