a7cef4151795351ec9b1b28b640be38ea6ec8347
Divakar Viswanath <divakar@HP-a6400z>
2014-03-13 21:13:30 -0400

    slapped copyright notice on all source files except python/asm

--- proc-microk/asm4xnx4.hh
+++ proc-microk/asm4xnx4.hh
@@ -1,3 +1,18 @@
+/*
+ * Copyright Divakar Viswanath, 2009-2014
+ */
+
+/*     
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as 
+ * published by the Free Software Foundation.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #ifndef __asm4xnx4_1Oct2013__
 #define __asm4xnx4_1Oct2013__
 
--- xphi-fft/test_fft.cpp
+++ xphi-fft/test_fft.cpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright Divakar Viswanath, 2009-2014
+ */
+
+/*     
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as 
+ * published by the Free Software Foundation.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include "../utils/utils.hh"
 #include "fft.hh"
 #include <cmath>
