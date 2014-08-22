5c9147341f1dbd69c458feabfff5451c8812ad20
Divakar Viswanath <divakar@tacc>
2014-08-21 19:56:54 -0500

    mpi/init, mpi/intro, mpi/exchange and utils working with new zmake

--- mpi/init/rules.mk
+++ mpi/init/rules.mk
@@ -3,25 +3,22 @@ saved := $(D)
 D := $(R)/mpi/init
 
 #########
-MPIINC 	 := `mpiCC -showme:compile`
-FFTWINC  := -I $$FFTW_INC
-MKLINC := -mkl
-DVMPIINC := `$$HOME/openmpi-1.6.3/bin/mpiCC -showme:compile`
-
-#########
-CPP 	 := icpc
-$(D)CFLAGS := -O3 -prec-div -no-ftz -restrict -Wshadow -MMD -MP
-CFLAGSXX := $(MPIINC)
-$(D)CFLAGS := $($(D)CFLAGS) $(CFLAGSXX)
-
+$(D)CFLAGS := $(CFLAGS) $(MPIINC)
 
 #########
 $(D)/%.o: $(D)/%.cpp
-	$(CPP) $($(@D)CFLAGS) $($(@D)EXTRNL) -o $@ -c $<
+	$(eval TMPTMP = $(CPP) $($(@D)CFLAGS) $($(@D)EXTRNL) -o $@ -c $<)
+	@echo $(subst $(R)/, , $(TMPTMP))
+	@ $(TMPTMP)
 $(D)/%.s: $(D)/%.cpp 
-	$(CPP) $($(@D)CFLAGS) -fno-verbose-asm  $($(@D)EXTRNL) -o $@ -S $< 
+	$(eval TMPTMP = $(CPP) $($(@D)CFLAGS) -fno-verbose-asm  \
+	$($(@D)EXTRNL) -o $@ -S $<)
+	@echo $(subst $(R)/, , $(TMPTMP))
+	@ $(TMPTMP)
 $(D)/%.o: $(D)/%.s 
 	$(CPP) $($(@D)CFLAGS) $($(@D)EXTRNL) -o $@ -c $< 
+	@echo $(subst $(R)/, , $(TMPTMP))
+	@ $(TMPTMP)
 
 #########
 $(D)/mpi_init.o: $(D)/mpi_init.cpp
