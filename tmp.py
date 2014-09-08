d314ea1cd1d9e84d88753a3b578b36f3f7ce337b
Divakar Viswanath <divakar@stampede>
2014-09-08 18:48:44 -0500

    modified Makefiles to replace obj by objl tmp.py script will be edited and then removed

---
+++ tmp.py
@@ -0,0 +1,17 @@
+import os
+pwd = os.path.abspath('.')
+for path, dlist, flist in os.walk(pwd, topdown=True):
+    if dlist.count('.git') > 0:
+        dlist.remove('.git')
+    if flist.count('Makefile') >= 1:
+        fname =  path+'/'+'Makefile'
+        fi = open(fname,'r')
+        fo = open('tmppp', 'w')
+        for l in fi:
+            ll = l.replace('obj', 'objl')
+            fo.write(ll)
+        fo.close()
+        fi.close()
+        os.system('mv tmppp '+ fname);
+os.system('rm tmppp')
+
