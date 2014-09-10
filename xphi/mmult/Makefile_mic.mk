46233ebe14299eb69e76d9267c7b5252c2b57d6a
Divakar Viswanath <divakar@stampede>
2014-09-10 12:04:14 -0500

    Makefiles in xphi/mmult separated for offload and native 	modified:   xphi/mmult/Makefile 	modified:   xphi/mmult/Makefile_mic.mk

--- xphi/mmult/Makefile_mic.mk
+++ xphi/mmult/Makefile_mic.mk
@@ -1,19 +1,22 @@
 include ../makevars.mk
-CFLAGSXX := -openmp $(MKLINC) #-offload-attribute-target=mic
-LIBS := -openmp $(MKLTHRD)
-MMIC := -mmic
-export MMIC
-MODS := ../utils ../xphi-init
+CFLAGS := $(CFLAGS) $(MKLINC) #-offload-attribute-target=mic
+LIBS := $(LIBS) $(MKLTHRD)
+EXTRNL := -mmic
+export EXTRNL
+MODS := ../../utils ../init
 
 
-##########
+######
 .PHONY: $(MODS)
 $(MODS):
-	make --directory=$@ all
+	make --directory=$@ objl
 
-#########
+######
+mmult.o: mmult.cpp
+-include mmult.d
 time_mic.o: time_mic.cpp
 -include time_mic.d
 
-time_mic.exe: time_mic.o mmult.o			\
-	       ../utils ../utils/utils.o ../utils/Table.o	
+######
+time_mic.exe: time_mic.o mmult.o					\
+	       ../../utils ../../utils/utils.o ../../utils/Table.o	
