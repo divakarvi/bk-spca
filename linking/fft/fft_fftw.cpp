3ae55822352057e9f402071bb1e000c43113d2d3
Divakar Viswanath <divakar@screms3.xphi>
2015-06-03 07:19:25 -0400

    fftw with 32 byte alignment. Since this is linked against MKL it should make no diff and it does not.
    	modified:   fft_fftw.cpp
    	modified:   fft_fftw.hh
    	modified:   output/ymm_time_fft_bwd_withoutfftw.txt

--- linking/fft/fft_fftw.cpp
+++ linking/fft/fft_fftw.cpp
@@ -18,7 +18,7 @@
 fft_fftw::fft_fftw(int nin)
   :n(nin)
 {
-	double *f = (double *)_mm_malloc(2*n*sizeof(double), 16);
+	double *f = (double *)_mm_malloc(2*n*sizeof(double), 32);
 	fftw_complex *ff  = (fftw_complex *)f;
 	pf = fftw_plan_dft_1d(n, ff, ff, -1, FFTW_MEASURE);
 	pb = fftw_plan_dft_1d(n, ff, ff,  1, FFTW_MEASURE);
