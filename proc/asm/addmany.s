e19cbcceebd6677fa8378b67b6f9d31caaba3405
Divakar Viswanath <divakar@TS-P300>
2015-06-12 09:50:26 -0400

    modified:   addmany.s

--- proc/asm/addmany.s
+++ proc/asm/addmany.s
@@ -7,6 +7,10 @@
 	.type	main, @function
 main:
 	subq	$32, %rsp
+	movl	$36, 24(%rsp)
+	movl	$8, 16(%rsp)
+	movl	$7, 8(%rsp)
+	movl	$6, (%rsp)
 	movl	$5, %r9d
 	movl	$4, %r8d
 	movl	$3, %ecx
@@ -14,10 +18,6 @@ main:
 	movl	$1, %esi
 	movl	$.LC0, %edi
 	xorl	%eax, %eax
-	movl	$36, 24(%rsp)
-	movl	$8, 16(%rsp)
-	movl	$7, 8(%rsp)
-	movl	$6, (%rsp)
 	call	printf
 	addq	$32, %rsp
 	ret
